4c6f69ee1587f8f0ca08d12a6173c6dba164fb3a
jyot <jyot@scytaleindia.com>
2023-02-17 12:13:37 +0530

    dragable logo done

--- lib/UI/posterPreview.dart
+++ lib/UI/posterPreview.dart
@@ -10,6 +10,8 @@ class PosterPreview extends StatefulWidget {
 
 class _PosterPreviewState extends State<PosterPreview> {
   final dragController = DragController();
+  Map<String, double> posLogo = {'top': 8, 'left': 8};
+  bool isDrag = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,15 +26,64 @@ class _PosterPreviewState extends State<PosterPreview> {
                 Image.asset("assets/frames/frame2.png"),
               ],
             ),
-            DraggableWidget(
-              initialPosition: AnchoringPosition.bottomLeft,
-              dragController: dragController,
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Container(
-                    height: 60,
-                    width: 60,
-                    child: Image.asset("assets/logo.png")),
+            AnimatedPositioned(
+              top: posLogo['top'],
+              left: posLogo['left'],
+              duration: Duration(milliseconds: isDrag ? 0 : 300),
+              child: Draggable(
+                // initialPosition: AnchoringPosition.bottomLeft,
+                // dragController: dragController,
+                onDragStarted: () {
+                  setState(() {
+                    isDrag = true;
+                  });
+                },
+                onDragUpdate: (details) {
+                  setState(() {
+                    print(details.delta.dx);
+                    print(details.delta.dy);
+                    posLogo = {
+                      'left': posLogo['left']! + details.delta.dx,
+                      'top': posLogo['top']! + details.delta.dy,
+                    };
+                  });
+                },
+                onDragEnd: (details) {
+                  double width = MediaQuery.of(context).size.width;
+                  double height = MediaQuery.of(context).size.width;
+
+                  setState(() {
+                    isDrag = false;
+                    posLogo = {
+                      'left': posLogo['left']!,
+                      // < (width - (150 / 2) - (12 * 2)) / 2
+                      //     ? 12
+                      //     : width - 110 - 12,
+                      'top': posLogo['top']!
+                      // < (height - (150 / 2) - (12 * 2)) / 2
+                      //     ? 12
+                      //     : height - 12,
+                    };
+                  });
+                },
+                childWhenDragging: Container(),
+
+                feedback: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Container(
+                      height: 60,
+                      width: 60,
+                      child: Image.asset("assets/logo.png")),
+                ),
+                child: isDrag
+                    ? Container()
+                    : Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Container(
+                            height: 60,
+                            width: 60,
+                            child: Image.asset("assets/logo.png")),
+                      ),
               ),
             )
             // Align(
